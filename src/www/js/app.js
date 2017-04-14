49cac2b0d9266db0ca3d96c5ebf282bad6257615
Eric W. Greene <eric@ericwgreene.com>
2017-04-14 09:57:46 -0700

    added code demos

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -1,4 +1,28 @@
-//import 'bootstrap-loader';
-//import '../scss/styles.scss';
+import 'bootstrap-loader';
+import '../scss/styles.scss';
 
-//console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ColorTool extends React.Component {
+
+    render() {
+        // return React.createElement('h1', null, 'Hello World');
+
+        // <h1>Hello World</h1>
+        return <div>
+            <h1>Color Tool</h1>
+            <ul>
+                <li>green</li>
+                <li>white</li>
+                <li>saffron</li>
+                <li>red</li>
+                <li>blue</li>
+                <li>black</li>
+            </ul>
+        </div>;
+    }
+
+}
+
+ReactDOM.render(<ColorTool />, document.querySelector('main'));
